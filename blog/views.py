1e0fe2d6aba6ee32304239cb3a9e9c9c929f9b96
desperado9371 <ch930701@gmail.com>
2020-02-25 18:45:16 +0900

    t

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ import ta
 def post_list(request):
     client = Client('ng0Zhq6ea42X3QxMV2RAubZxs508gguTISRwM13lQFFPrDDTxRiqmq3pBvIcvJMy',
                     'vHZDWuQvPf4mcaDvzxwRbtIWDbWuCyFyyG59bCZeTW6A6sd98qbHfCsFDVDdN3wn')
-    klines = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "6 month ago UTC")
+    klines = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "1 month ago UTC")
     unix_timestamps = []
     for kline in klines:
         unix_timestamps.append(kline[0])
@@ -32,14 +32,17 @@ def post_list(request):
         volume.append(float(kline[5]))
 
     for unix_timestamp in unix_timestamps:
-        timestamps.append(datetime.fromtimestamp(unix_timestamp / 1000).strftime("%m/%d/%Y, %H:%M:%S"))
+        timestamps.append(datetime.fromtimestamp(unix_timestamp / 1000).strftime("%m/%d/%Y"))
 
     for kline in klines:
         unix_timestamps.append(kline[0])
 
+    candledata = []
+    for i in range(len(timestamps)):
+        candledata.append([timestamps[i],low_prices[i],open_prices[i],closed_prices[i],high_prices[i]])
 
-    data=[1,2,3,4,5,]
-    print(closed_prices)
-    print(timestamps)
+    print(candledata)
+
     return render(request, 'blog/post_list.html', {'data': closed_prices,
-                                                   'labels': timestamps})
+                                                   'labels': timestamps,
+                                                   'candle':candledata,})
